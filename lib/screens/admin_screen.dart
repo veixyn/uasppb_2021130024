80d4b7d69c31d75e8f6d630f682d83f00fde5550
Tangana Vito Fortunata <tanganavito321@gmail.com>
2024-11-08 05:51:42 +0700

    Modify admin_screen.dart to be similiar to upcoming_events.dart.

--- lib/screens/admin_screen.dart
+++ lib/screens/admin_screen.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:uasppb_2021130024/screens/add_event_screen.dart';
+import 'package:uasppb_2021130024/screens/admin_screen.dart';
+import 'package:uasppb_2021130024/screens/event_details.dart';
 import 'package:uasppb_2021130024/screens/login_screen.dart';
+import 'package:uasppb_2021130024/screens/my_events.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class AdminScreen extends StatelessWidget {
   @override
@@ -11,19 +19,68 @@ class AdminScreen extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SignOutScreen()
+      home: HomePage(),
     );
   }
 }
 
-class SignOutScreen extends StatefulWidget {
-  const SignOutScreen({super.key});
-
+class HomePage extends StatefulWidget {
   @override
-  State<SignOutScreen> createState() => _SignOutScreenState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _SignOutScreenState extends State<SignOutScreen> {
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  // List of screens to navigate to
+  final List<Widget> _pages = [
+    AllEvents(),
+    AddEventForm(),
+    MyEventsScreen(),
+  ];
+
+  // Function to handle navigation
+  void _onItemTapped(int index) async {
+    if (index == 3) {
+      // Show a confirmation dialog before sign-out
+      _confirmSignOut(context);
+    } else {
+      // Update selected index for other items
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
+// Function to confirm sign-out
+  void _confirmSignOut(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Confirm Sign Out"),
+          content: const Text("Are you sure you want to sign out?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                _signOut(context); // Proceed with sign-out
+              },
+              child: const Text("Sign Out"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+// Function to perform the sign-out
   Future<void> _signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     // Navigate back to the LoginScreen
@@ -37,16 +94,147 @@ class _SignOutScreenState extends State<SignOutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text("placeholder admin screen"),
-          ElevatedButton(
-            onPressed: () => _signOut(context), // Pass context to _signOut
-            child: const Text("Sign Out"),
+      appBar: AppBar(
+        title: const Text('Dicoding Events'),
+      ),
+      body: _pages[_selectedIndex], // Display the selected page
+
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex, // Track the selected index
+        onTap: _onItemTapped, // Handle item tap
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event_available),
+            label: 'All Events',
           ),
-        ]
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check),
+            label: 'Add New Event',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'My Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: 'Logout',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// "Upcoming Events" Page
+class AllEvents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'All Events',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Expanded(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('events').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                return const Center(child: Text('No events found.'));
+              }
+              final events = snapshot.data!.docs;
+
+              return ListView.builder(
+                itemCount: events.length,
+                itemBuilder: (context, index) {
+                  final event = events[index];
+                  return EventCard(
+                    title: event['eventName'] ?? 'No Title',
+                    summary: event['summary'] ?? 'No Summary',
+                    host: event['eventHost'] ?? 'Unknown Host',
+                    startingTime: event['startingTime'] ?? 'No Time',
+                    quota: event['quota'] ?? 0,
+                    imageBase64: event['imageBase64'],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EventDetailsScreen(
+                            title: event['eventName'] ?? 'No Title',
+                            summary: event['summary'] ?? 'No Summary',
+                            host: event['eventHost'] ?? 'Unknown Host',
+                            startingTime: event['startingTime'] ?? 'No Time',
+                            quota: event['quota'] ?? 0,
+                            imageBase64: event['imageBase64'],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class EventCard extends StatelessWidget {
+  final String title;
+  final String summary;
+  final String host;
+  final String startingTime;
+  final int quota;
+  final String? imageBase64;
+  final VoidCallback onTap;
+
+  const EventCard({
+    Key? key,
+    required this.title,
+    required this.summary,
+    required this.host,
+    required this.startingTime,
+    required this.quota,
+    this.imageBase64,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Card(
+        elevation: 2.0,
+        child: ListTile(
+          leading: imageBase64 != null
+              ? Image.memory(
+            base64Decode(imageBase64!),
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+          )
+              : Container(
+            width: 50,
+            height: 50,
+            color: Colors.grey[300],
+          ),
+          title: Text(title),
+          subtitle: Text(summary),
+          onTap: onTap,
+        ),
       ),
     );
   }
